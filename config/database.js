9946ecb248388e5bb409022b6989d0ea7a3b5f88
edengebrezgi <edengebrezgi@gmail.com>
2021-04-22 17:00:26 -0400

    initial commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://edengebrezgi:aaliyah@cluster0.fakzp.mongodb.net/injera?retryWrites=true&w=majority', 
+    'dbName': 'injera'
+};
